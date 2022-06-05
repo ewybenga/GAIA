768c79e2209a490dbf425070d30bbeb8f439b706
Eric Wybenga <e.wybenga@gmail.com>
2022-06-05 01:03:06 -0700

    updating arduino communication code to accept positional arguments

--- Python/arduino_comm.py
+++ Python/arduino_comm.py
@@ -1,20 +1,49 @@
 # https://pyserial.readthedocs.io/en/latest/shortintro.html
 # https://create.arduino.cc/projecthub/ansh2919/serial-communication-between-python-and-arduino-e7cce0#:~:text=check%20your%20installation.-,Step%203%3A%20Python%20Code,-First%20up%2C%20we
 
+import argparse
 import serial
 import time
+# import pandas as pd
 
-def main():
-    num = input("Enter a number: ") # Taking input from user
-    arduino.write(bytes(num, 'utf-8'))
-    time.sleep(0.05)
-    for i in range(5):
-        data = arduino.readline()
-        if data==b'':
-            break
-        print(data)
+# TODO
+# install conda on pi
+# create Environment with requirement.txt
+# install pandas to it
+
+COMMAND_MAP = {
+    1: "Collect all data [Soil moisture %]",
+    2: "Collect Soil Moisture Percent"
+}
+
+def main(commands):
+    
+    for command in commands:
+        have_info=False
+        print(f"Waiting for data from command: {COMMAND_MAP[command]}...")
+        while not have_info:
+            print("...")
+            arduino.write(bytes(str(command), 'utf-8'))
+            time.sleep(0.05)
+            while True:
+                data = arduino.readline()
+                if data==b'':
+                    break
+                plant_data = data
+                have_info=True
+            if data is None:
+                have_info = False
+        plant_data = plant_data.decode('UTF-8').split(',')[:-1]
+        # print(plant_data.split(',')[:-1])
+        print(plant_data)
 
 
 if __name__ == "__main__":
-    arduino = serial.Serial('COM3', 9600, timeout=1)
-    main()
+    parser = argparse.ArgumentParser(description='Request data from the plant monitor.')
+    parser.add_argument('commands', type=int, nargs='+',
+                    help='The list of commands to send to the arduino separated by spaces ')
+
+    args = parser.parse_args()
+    print(f"Processing the following commands: {args.commands}")
+    arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    main(args.commands)
