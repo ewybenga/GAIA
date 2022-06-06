1a7139f1f66bbc1deb4af0aaade95b5fb73bcd7f
Eric Wybenga <e.wybenga@gmail.com>
2022-06-06 01:38:00 -0700

    adding support for yaml config file and verbosity levels. adding comments and raise value error if not one of the supported commands

--- Python/arduino_comm.py
+++ Python/arduino_comm.py
@@ -1,49 +1,53 @@
-# https://pyserial.readthedocs.io/en/latest/shortintro.html
-# https://create.arduino.cc/projecthub/ansh2919/serial-communication-between-python-and-arduino-e7cce0#:~:text=check%20your%20installation.-,Step%203%3A%20Python%20Code,-First%20up%2C%20we
-
 import argparse
 import serial
 import time
 import pandas as pd
+import yaml
 
-# TODO:
-# import yaml and put the port and command map in a config.yaml file
-COMMAND_MAP = {
-    1: "Collect all data [Soil moisture %]",
-    2: "Collect Soil Moisture Percent"
-}
 
-def main(commands):
-    
-    for command in commands:
-        if command not in COMMAND_MAP.keys():
+def main(args, config):
+    if args.verbose > 0:  # This print statement will only appear if the -v flag is set
+        print(f"Processing the following commands: {args.commands}")
+
+    for command in args.commands:
+        if command not in config['command_map'].keys():  # Check that we received a valid command
             print(f"Unrecognized command: {command}")
+            raise ValueError
             continue
+
         have_info=False
-        print(f"Waiting for data from command: {COMMAND_MAP[command]}...")
+
+        if args.verbose > 0:
+            print(f"Waiting for data from command: {config['command_map'][command]}",end='',flush=True)
+
         while not have_info:
-            print("...")
-            arduino.write(bytes(str(command), 'utf-8'))
-            time.sleep(0.05)
-            while True:
-                data = arduino.readline()
-                if data==b'':
-                    break
-                plant_data = data
+            if args.verbose > 0:  
+                print(".", sep='', end='', flush=True)
+
+            arduino.write(bytes(str(command), 'utf-8'))  # Send our command to the Arduino
+            time.sleep(0.05)  # Wait for data to arrive
+            data = arduino.readline()  # Read what was sent from the Arduino
+
+            if data!=b'':  # Break condition: when we receive non-empty data we will record it and break the while loop
+                data = data.decode('UTF-8').split(',')[:-1]
                 have_info=True
-            if data is None:
-                have_info = False
-        plant_data = plant_data.decode('UTF-8').split(',')[:-1]
-        # print(plant_data.split(',')[:-1])
-        print(plant_data)
+
+        if args.verbose > 0:
+            print(" done!")
+            if args.verbose > 1:
+                print(data)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Request data from the plant monitor.')
     parser.add_argument('commands', type=int, nargs='+',
                     help='The list of commands to send to the arduino separated by spaces ')
-
+    parser.add_argument('--verbose', '-v', action='count', default=0)
     args = parser.parse_args()
-    print(f"Processing the following commands: {args.commands}")
-    arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    main(args.commands)
+
+    with open('config.yaml') as f:
+        config = yaml.safe_load(f)
+
+    arduino = serial.Serial(config['port'], 9600, timeout=1)
+
+    main(args, config)
